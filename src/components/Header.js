f9a4f6de3ce8d2e4eadcb57cbb1c3de306536478
samuelsit <samsitruk@gmail.com>
2020-04-06 04:30:07 +0200

    Login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import logo from '../img/logo.png'
+import logoText from '../img/logoText.png'
 import '../css/Header.css'
 
 class Header extends Component {
 
     state = {
-        isAuth: false
+        isAuth: false,
+        width: window.innerWidth
     }
 
     handleClick = () => {
-        this.setState(this.state.isAuth === false ? {isAuth: true} : {isAuth: false})
+        this.setState(this.state.isAuth === false ? {isAuth: true} : {isAuth: false})        
     }
 
     render () {
@@ -17,9 +19,9 @@ class Header extends Component {
             return (
                 <header>
                     <nav className="navbar navbar-expand navbar-light bg-light shadow fixed-top" id="navbar">
-                            <div className="navbar-brand">
-                                <img src={logo} width="200" className="d-inline-block align-top" alt="logo"/>
-                            </div>
+                        <div className="navbar-brand logotext">
+                            <img src={logoText} height="40" className="d-inline-block align-top" alt="logo"/>
+                        </div>
                         <div className="collapse navbar-collapse">
                             <ul className="navbar-nav">
                                 <li className="nav-item"></li>
@@ -34,18 +36,21 @@ class Header extends Component {
             return (
                 <header>
                     <nav className="navbar navbar-expand navbar-light bg-light shadow fixed-top" id="navbar">
-                            <div className="navbar-brand">
-                                <img src={logo} width="200" className="d-inline-block align-top" alt="logo"/>
-                            </div>
+                        <div className="navbar-brand logoAuth">
+                            <img src={logo} height="40" className="d-inline-block align-top" alt="logo"/>
+                        </div>
+                        <div className="navbar-brand logoAuthText">
+                            <img src={logoText} height="40" className="d-inline-block align-top" alt="logo"/>
+                        </div>
                         <div className="collapse navbar-collapse">
                             <ul className="navbar-nav">
                                 <li className="nav-item"></li>
                             </ul>
                         </div>
-                        <form className="form-inline d-flex justify-content-center md-form form-sm mt-2">
-                        <button type="submit" className="btn btn-danger btn-sm text-light" value="SEARCH"><i className="fas fa-search"></i></button>
-                        <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="" aria-label="Search" />
-                        </form>
+                        <div className="searchbar mr-1">
+                            <input className="search_input" type="text" placeholder="Search..."/>
+                            <div className="search_icon"><i className="fas fa-search"></i></div>
+                        </div>
                         <div className="text-light btn btn-light mr-2"><i className="fas fa-heart text-danger"></i></div>
                         <div className="text-light btn btn-secondary mr-2"><i className="fas fa-user"></i></div>
                         <div className="text-light btn btn-danger mr-2" onClick={this.handleClick}>Déconnexion</div>
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Header from './Header'
+import '../css/Registration.css'
+import galerie from '../img/galerie.png'
 
 class Registration extends Component {
 
     _isMounted = false;
 
     componentDidMount() {
+        this.slideGalerie()
         this._isMounted = true
     }
 
@@ -13,9 +16,62 @@ class Registration extends Component {
         this._isMounted = false;
     }
 
+    slideGalerie = () => {
+        const galerie = document.getElementById("galerie")
+        let top = 0
+        setInterval(() => {
+            if (top === -900) {
+                top = 0
+            }
+            else {
+                top -= 0.5
+            }
+            galerie.style.top = top + 'px'
+            console.log(document.getElementById("galerie").style.top);            
+        }, 20)
+    }
+
     render () {
         return (
-            <Header/>
+            <Fragment>
+                <Header/>
+                <div className="login-block">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-4 login-sec">
+                                <h2 className="text-center">Connectez vous !</h2>
+                                <form className="login-form">
+                                    <div className="form-group">
+                                        <label htmlFor="exampleInputEmail1" className="text-uppercase">Pseudo</label>
+                                        <input type="text" className="form-control" placeholder="" id="exampleInputEmail1"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="exampleInputPassword1" className="text-uppercase">Mot de passe</label>
+                                        <input type="password" className="form-control" placeholder="" id="exampleInputPassword1"/>
+                                    </div>
+                                    <div className="form-check">
+                                        <button type="submit" className="btn btn-login float-right">Connexion</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="col-md-8 banner-sec">
+                                <div id="" className="carousel slide">
+                                    <div className="carousel-inner">
+                                        <div className="active">
+                                            <img id="galerie" className="d-block img-fluid" src={galerie} alt="First slide"/>
+                                            <div className="carousel-caption d-none d-md-block">
+                                                <div className="banner-text">
+                                                    <h2 className="contour-titre">HyperTube</h2>
+                                                </div>	
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
         )
     }
 }
